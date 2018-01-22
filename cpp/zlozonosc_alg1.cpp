584c88666b8d879033bf1bb118317d8ee070e340
w_si2000 <wiktoria.sierant1508@interia.pl>
2018-01-22 11:56:59 +0100

    nieparzyste.cpp

---
+++ cpp/zlozonosc_alg1.cpp
@@ -0,0 +1,19 @@
+/*
+ * zlozonosc_alg1.cpp
+ */
+
+
+#include <iostream>
+
+using namespace;
+
+int main(int argc, char **argv)
+{
+	int a;
+    cout << "Podaj wartość liczby a:";
+    cin >> a;
+    for(int a > 0; a >100)
+        
+	return 0;
+}
+
